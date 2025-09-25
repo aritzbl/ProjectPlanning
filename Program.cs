7e0c24c0bc1fe1e33acf3d387e9e651f2b86271c
Irina Lamboglia <irinalamboglia@gmail.com>
2025-09-25 19:05:13 -0300

    no subir

--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
-using ProjectPlanning.Web.Services; // 👈 importante
+using System.Net;
+using ProjectPlanning.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-.
 builder.Services.AddControllersWithViews();
 
-
-builder.Services.AddHttpClient<IBonitaApiService, BonitaApiService>();
+builder.Services.AddHttpClient<IBonitaApiService, BonitaApiService>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["Bonita:BaseUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+{
+    UseCookies = true,
+    CookieContainer = new CookieContainer()
+});
 
 var app = builder.Build();
 
-.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -18,15 +24,12 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();          // sirve archivos estáticos (wwwroot)
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
